9d9bc2dfb47bdecee786aa5e1b2347b5114502e2
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-10-25 23:47:48 +0600

    add navber

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+
   plugins: [
     require('daisyui'),
   ],
